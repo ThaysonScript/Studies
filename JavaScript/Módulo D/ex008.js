86c48171ebbaddb7ad0ab13edd1cefed1e93de90
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2023-01-10 20:31:33 -0300

    Add files via upload

---
+++ JavaScript/Módulo D/ex008.js
@@ -0,0 +1,8 @@
+//  CONDICIONAL SIMPLES
+
+var vel = 7
+
+if (vel >= 60) {
+    console.log(`A velocidade registrada foi ${vel} km/h, voce ultrapassou a velocidade de 60 km/h e foi Multado!`)
+}
+console.log(`Dirija com cuidado!`)
