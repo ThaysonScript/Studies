3c7c107a76e57818c592e712c4aca2f9ab4367b8
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2023-02-11 02:00:56 -0300

    Update Principal.cs

--- C#/Principal.cs
+++ C#/Principal.cs
@@ -1,4 +1,9 @@
 using System;
-namespace Principal;
 
- 
+namespace HelloWorld {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
