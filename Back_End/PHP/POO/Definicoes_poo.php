12f35a3a4b866d6225800a6a9d3dba007dbaefbe
Thayson Guedes <thaysonguedes01@gmail.com>
2023-02-12 15:17:00 -0300

    Update Definicoes_poo.php

--- Back_End/PHP/POO/Definicoes_poo.php
+++ Back_End/PHP/POO/Definicoes_poo.php
@@ -9,11 +9,19 @@
 
         //metodos/acoes/funcoes
         function set_name($name) {
-            $this->name = $name;
+            $this -> name = $name;
         }
 
         function get_name() {
-            return $this->name;
+            return $this -> name;
+        }
+
+        function set_color($color) {
+            $this -> color = $color;
+        }
+
+        function get_color() {
+            return $this -> color;
         }
 
     }
@@ -21,12 +29,16 @@
     //criar nova instancia da classe/clonar esqueleto
     $apple = new Fruit();
     $apple->set_name('Apple');
+    $apple -> set_color('Red');
 
     $banana = new Fruit();
     $banana->set_name('Banana');
+    $banana -> set_color("Yellow");
 
     echo $apple->get_name();
+    echo $apple->get_color();
     echo "<br>";
     echo $banana->get_name();
+    echo $banana->get_color();
 
 ?>
