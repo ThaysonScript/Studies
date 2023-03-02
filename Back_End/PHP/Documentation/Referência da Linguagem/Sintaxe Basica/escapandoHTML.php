5eee2916671ce90ba56d70e6d66a1964537fb73d
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2023-03-02 10:53:39 -0300

    php started

---
+++ Back_End/PHP/Documentation/Referência da Linguagem/Sintaxe Basica/escapandoHTML.php
@@ -0,0 +1,10 @@
+<p>
+    Isto vai ser ignorado pelo PHP e exibido pelo navegador
+</p>
+<?php 
+    echo 'Enquanto isto vai ser interpretado.';
+    //
+?>
+<p>
+    Isto também vai ser ignorado pelo PHP e exibido no navegador.
+</p>
