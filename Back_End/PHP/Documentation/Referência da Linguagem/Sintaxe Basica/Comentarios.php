0a00d4e08762e9b2fac8d3931ef13542340cb798
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2023-03-02 11:42:38 -0300

    php

---
+++ Back_End/PHP/Documentation/Referência da Linguagem/Sintaxe Basica/Comentarios.php
@@ -0,0 +1,12 @@
+<?php
+    echo 'Isto é um teste'; // Estilo de comentário de uma linha em  C++
+    /* Este é um comentário de múltiplas linhas
+       ainda outra linha de comentário */
+    echo 'Isto é ainda outro teste';
+    echo 'Um teste final'; # Este é um comentário de uma linha no estilo shell
+?>
+
+
+<h1>Isto é um <?php # echo 'simples';     tudo que tiver aqui dentro sera ignorado pelo browser?> exemplo.</h1>
+<p>O cabeçalho acima irá dizer 'Isto é um exemplo'.</p>
+
--- Back_End/PHP/Documentation/Referência da Linguagem/Sintaxe Basica/escapandoHTML.php
+++ Back_End/PHP/Documentation/Referência da Linguagem/Sintaxe Basica/escapandoHTML.php
@@ -3,8 +3,20 @@
 </p>
 <?php 
     echo 'Enquanto isto vai ser interpretado.';
-    //
+    // o interpretador php só lerá o que tá dentro das tags php enquanto o resto fora será interpretado pelo navegador
 ?>
 <p>
     Isto também vai ser ignorado pelo PHP e exibido no navegador.
 </p>
+
+
+Exemplo #1 Escape avançado usando condições
+<?php if ($expression == true): // definir laço ?>
+
+    Isto irá aparecer se a expressão for verdadeira.
+
+<?php else: // senao ?>
+
+    Senão isso que aparecerá.
+
+<?php endif; // para parar o laço, indicar o seu fim ?>
